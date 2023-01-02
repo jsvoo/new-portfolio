5e6b6f42c8780b42f60dc49af29857c1287332d1
Onoja Victor <onoja.jsdev@gmail.com>
2023-01-02 19:07:51 +0000

    Replaced all link tags with divs. Used ternary operatos to switch components in place of routes

--- src/components/pages/Navigation.js
+++ src/components/pages/Navigation.js
@@ -1,5 +1,8 @@
 import "../styles/navigation.scss";
-
+import HomePage from "./HomePage.js"
+import Contact from "./Contact.js"
+import About from "./About.js"
+import Portfolio from "./Portfolio.js"
 import { FaHome, FaUserAlt, FaEnvelopeOpen } from "react-icons/fa";
 import { VscColorMode } from "react-icons/vsc";
 import { MdWork } from "react-icons/md";
@@ -11,10 +14,13 @@ export default function Navigation() {
   const { initial, setinitial, clicked, setClicked } = useContext(
     contextManager
   );
-  return (
+  return ( 
+    
+    <>
     <div
       className={initial.light_mode ? "nav-items n-light-mode" : "nav-items"}
     >
+      
       <div
         className={initial.light_mode ? "green-ico mode" : "ico mode"}
         onClick={() =>
@@ -24,34 +30,48 @@ export default function Navigation() {
         <VscColorMode />
       </div>
 
-      <Link
-        to="/"
+      <div
+        // to="/"
         className={clicked.home ? "green-icon" : "icon"}
         onClick={() => setClicked({ ...initial, home: true })}
       >
         <FaHome />
-      </Link>
-      <Link
-        to="/about"
+      </div>
+      <div
+        // to="/about"
         className={clicked.about ? "green-icon" : "icon"}
         onClick={() => setClicked({ ...initial, about: true })}
       >
         <FaUserAlt />
-      </Link>
-      <Link
-        to="portfolio"
+      </div>
+      <div
+        // to="portfolio"
         className={clicked.portfolio ? "green-icon" : "icon"}
         onClick={() => setClicked({ ...initial, portfolio: true })}
       >
         <MdWork />
-      </Link>
-      <Link
-        to="/contact"
+      </div>
+      <div
+        // to="/contact"
         className={clicked.contact ? "green-icon" : "icon"}
         onClick={() => setClicked({ ...initial, contact: true })}
       >
         <FaEnvelopeOpen />
-      </Link>
+      </div>
     </div>
+    
+      {
+      clicked.home?(<HomePage/>):null
+      }
+      {
+      clicked.about?(<About/>):null
+      }
+      {
+      clicked.portfolio?(<Portfolio/>):null}
+      {
+      clicked.contact?(<Contact/>):null
+      }
+    </>
+     
   );
 }
